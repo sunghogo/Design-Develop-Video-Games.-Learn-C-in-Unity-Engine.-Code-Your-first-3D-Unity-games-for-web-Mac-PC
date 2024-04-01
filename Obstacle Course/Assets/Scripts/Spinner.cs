b169a32d84cc3c7c3949d571aa1870ff2008dab7
sunghogo <70732249+sunghogo@users.noreply.github.com>
2024-04-01 17:18:03 -0400

    S2: Obstacle Course L25: Rotate an Object Done

---
+++ Obstacle Course/Assets/Scripts/Spinner.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spinner : MonoBehaviour
+{
+    [SerializeField] private float _xAngle, _yAngle,_zAngle;
+
+    [SerializeField] private float _rotationSpeed;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _xAngle = 0;
+        _yAngle = 1;
+        _zAngle = 0;
+
+        _rotationSpeed = 10;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(_xAngle, _yAngle* Time.deltaTime * _rotationSpeed, _zAngle) ;
+    }
+}
