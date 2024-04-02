b537d35c409dcd3364911519f7aeffbb4eac72bf
sunghogo <70732249+sunghogo@users.noreply.github.com>
2024-04-02 12:24:44 -0400

    S3: Project Boost L39: Rigidbody Constraints Finish

---
+++ Project Boost/Assets/Scripts/Movement.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using UnityEngine.TextCore.LowLevel;
+
+public class Movement : MonoBehaviour
+{
+    private Rigidbody _rigidBody;
+
+    [SerializeField] private float _mainThrust;
+    [SerializeField] private float _rotationThrust;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidBody = GetComponent<Rigidbody>();
+        _rigidBody.drag = 0.25f;
+        _mainThrust = 1000f;
+        _rotationThrust = 100f;
+
+        // Constraints
+        _rigidBody.constraints = RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput() {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    private void ProcessThrust() {
+        if (Input.GetKey(KeyCode.Space)) {
+            ApplyThrust();
+        }
+    }
+
+    private void ProcessRotation() {
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            RotateLeft();
+        }
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            RotateRight();
+        }
+    }
+
+    private void RotateLeft() {
+        ApplyRotation(_rotationThrust);
+    }
+
+    private void RotateRight() {
+        ApplyRotation(-_rotationThrust);
+    }
+
+    private void ApplyRotation(float rotationThrust)
+    {
+        _rigidBody.freezeRotation = true;
+        transform.Rotate(UnityEngine.Vector3.forward * rotationThrust * Time.deltaTime);
+        _rigidBody.freezeRotation = false;
+    }
+
+    private void ApplyThrust() {
+        _rigidBody.AddRelativeForce(UnityEngine.Vector3.up * _mainThrust * Time.deltaTime);
+
+    }
+}
